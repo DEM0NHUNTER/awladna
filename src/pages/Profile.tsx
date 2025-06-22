36d70b54c1fcf7ceeb36c169780e6e38624b4b33
Krieg <potatoclan0@gmail.com>
2025-06-22 21:22:47 +0300

    Deploy updated frontend to Vercel

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from "../../../context/AuthContext";
 import axiosInstance from "../api/axiosInstance";
 
 const Profile: React.FC = () => {
