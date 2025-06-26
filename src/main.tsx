98269931e0124bf0f2a80159798bbf4926648e88
Krieg <potatoclan0@gmail.com>
2025-06-26 15:45:05 +0300

    Profile page

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,13 @@
-// src/main.tsx
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "next-themes";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        <AOSWrapper>
-          <App />
-        </AOSWrapper>
-      </ThemeProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-function AOSWrapper({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    AOS.init({ once: true, duration: 800, easing: "ease-in-out" });
-  }, []);
-  return <>{children}</>;
-}
