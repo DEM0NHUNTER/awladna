a81a5b99b9391f25badc03fc24f27249b525ca55
Krieg <potatoclan0@gmail.com>
2025-06-30 23:59:40 +0300

    revamp

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useAuthStore } from './store/authStore';
-import LoginPage from './pages/LoginPage';
-import ChatPage from './pages/ChatPage';
-import ChildProfilePage from './pages/ChildProfilePage';
+import { LoginPage } from './pages/LoginPage';
+import { ChatPage } from './pages/ChatPage';
+import { ChildProfilePage } from './pages/ChildProfilePage';
+import { RegisterPage } from './pages/RegisterPage';
 
 function App() {
   const { token } = useAuthStore();
