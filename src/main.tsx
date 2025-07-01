e15737184b149c7c177d6733b43f07702b19c24e
Krieg <potatoclan0@gmail.com>
2025-07-02 00:14:18 +0300

    revamp

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import ReactDOM from "react-dom/client";
+import React from "react";
+import App from './App';
+import './index.css';
+// Initialize i18next for internationalization
+
 
 i18n
   .use(Backend)
