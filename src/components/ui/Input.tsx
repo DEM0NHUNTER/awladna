6bb2f6ac8c6af8f017d4423d71cbe002e749898a
Krieg <potatoclan0@gmail.com>
2025-07-03 23:48:43 +0300

    Profile fixes

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,21 +1,27 @@
 // src/components/ui/Input.tsx
-import * as React from "react";
-import { cn } from "@/lib/utils";
+import React from "react";
+import clsx from "clsx";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type = "text", ...props }, ref) => (
-    <input
-      type={type}
-      className={cn(
-        "block w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm text-gray-900 shadow-sm placeholder:text-gray-400 focus:border-blue-500 focus:ring-blue-500 focus:outline-none focus:ring-1",
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={clsx(
+          "w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent disabled:opacity-50 disabled:cursor-not-allowed text-sm",
+          className
+        )}
+        {...props}
+      />
+    );
+  }
 );
 
 Input.displayName = "Input";
+
+export default Input;
+
