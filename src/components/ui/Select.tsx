46594cfaa1759601210a8d1d60e43139d9beed18
Krieg <potatoclan0@gmail.com>
2025-07-03 18:01:14 +0300

    Profile Utils

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,28 +1,74 @@
-// src/components/ui/Select.tsx
+"use client";
+
 import * as React from "react";
+import * as RadixSelect from "@radix-ui/react-select";
+import { Check, ChevronDown, ChevronUp } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-export interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
-  options: { label: string; value: string }[];
-}
+// Root
+export const Select = RadixSelect.Root;
 
-export const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
-  ({ className, options, ...props }, ref) => (
-    <select
+// Trigger
+export const SelectTrigger = React.forwardRef<
+  React.ElementRef<typeof RadixSelect.Trigger>,
+  React.ComponentPropsWithoutRef<typeof RadixSelect.Trigger>
+>(({ className, children, ...props }, ref) => (
+  <RadixSelect.Trigger
+    ref={ref}
+    className={cn(
+      "flex h-10 w-full items-center justify-between rounded-md border border-gray-300 bg-white px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-50",
+      className
+    )}
+    {...props}
+  >
+    {children}
+    <RadixSelect.Icon asChild>
+      <ChevronDown className="h-4 w-4 opacity-50" />
+    </RadixSelect.Icon>
+  </RadixSelect.Trigger>
+));
+SelectTrigger.displayName = RadixSelect.Trigger.displayName;
+
+// Value
+export const SelectValue = RadixSelect.Value;
+
+// Content
+export const SelectContent = React.forwardRef<
+  React.ElementRef<typeof RadixSelect.Content>,
+  React.ComponentPropsWithoutRef<typeof RadixSelect.Content>
+>(({ className, children, ...props }, ref) => (
+  <RadixSelect.Portal>
+    <RadixSelect.Content
       ref={ref}
       className={cn(
-        "block w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500",
+        "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-white text-sm shadow-md",
         className
       )}
       {...props}
     >
-      {options.map((opt) => (
-        <option key={opt.value} value={opt.value}>
-          {opt.label}
-        </option>
-      ))}
-    </select>
-  )
-);
+      <RadixSelect.Viewport className="p-1">{children}</RadixSelect.Viewport>
+    </RadixSelect.Content>
+  </RadixSelect.Portal>
+));
+SelectContent.displayName = RadixSelect.Content.displayName;
 
-Select.displayName = "Select";
+// Item
+export const SelectItem = React.forwardRef<
+  React.ElementRef<typeof RadixSelect.Item>,
+  React.ComponentPropsWithoutRef<typeof RadixSelect.Item>
+>(({ className, children, ...props }, ref) => (
+  <RadixSelect.Item
+    ref={ref}
+    className={cn(
+      "relative flex w-full cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-blue-100",
+      className
+    )}
+    {...props}
+  >
+    <RadixSelect.ItemText>{children}</RadixSelect.ItemText>
+    <RadixSelect.ItemIndicator className="absolute right-2 inline-flex items-center">
+      <Check className="h-4 w-4 text-blue-600" />
+    </RadixSelect.ItemIndicator>
+  </RadixSelect.Item>
+));
+SelectItem.displayName = RadixSelect.Item.displayName;
