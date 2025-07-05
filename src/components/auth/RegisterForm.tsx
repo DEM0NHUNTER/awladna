858e3835ef9ed76ed1f30ec5a3bd40925d3957f0
Krieg <potatoclan0@gmail.com>
2025-07-05 13:54:33 +0300

    Profile fixes

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -1,7 +1,7 @@
 // src/components/RegisterForm.tsx
 
 import React, { useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext"; // ✅ correct
 import { format } from "date-fns";
 import toast from "react-hot-toast";
 
